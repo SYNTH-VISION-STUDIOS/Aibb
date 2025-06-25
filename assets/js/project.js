606c0021675a790dbb2af9172d2fd9fad6975bed
Manarul Hidayat <146616690+SYNTH-VISION-STUDIOS@users.noreply.github.com>
2025-06-25 23:07:52 +0700

    Create project.js

---
+++ assets/js/project.js
@@ -0,0 +1,97 @@
+export const project = [
+  // {
+  //   id: 9,
+  //   title: "SPOTTA NG",
+  //   description:
+  //     "See through the <span>lenses of people</span> who have lived or visited the <span>neighbourhood you might have in mind.</span>",
+  //   category: "Design,",
+  //   others: "Figma",
+  //   img: "./assets/img/work9.png",
+  //   link: "https://bit.ly/3U2lMUN",
+  // },
+  {
+    id: 10,
+    title: "Kecantikannya Terlalu Jujur Sampai Kamera Menolak Fokus",
+    description:
+      "Wajahnya terlalu autentik sampai AI di kamera pun bingung harus memprosesnya sebagai manusia, hewan, atau objek tidak dikenal.</span>",
+    img: "./assets/img/a4.jpg",
+    imgAlt: "Softimi Company Logo",
+    imgClass: "rounded-lg shadow-md object-cover",
+    imgStyle: "max-height: 200px; width: auto;",
+  },
+  {
+    id: 9,
+    title: "Ini Bukan Foto, Ini Ancaman Sosial",
+    description:
+      "Foto ini bisa jadi alat pemerasan. Cocok dipakai buat barter diam di grup. Sekali sebar, satu circle bisa bubar.</span>.",
+    img: "./assets/img/a14.jpg",
+    link: "https://nakamurafc.com/",
+  },
+  {
+    id: 8,
+    title: "Ketika Tuhan Lagi Bercanda Pas Nyiptain Dia",
+    description:
+      "Setiap ciptaan Tuhan itu indah... tapi yang ini, mungkin lagi error jaringan surgawi waktu proses rendering-nya.</span>.",
+    img: "./assets/img/a13.jpg",
+    link: "https://www.figma.com/file/QdGMxn0FQWA5hDriMLdoSl/OptimuzHomez?type=design&node-id=0%3A1&mode=design&t=UWWMa35vNYAkS57A-1",
+    imgAlt: "Softimi Company Logo",
+    imgClass: "rounded-lg shadow-md object-cover",
+    imgStyle: "max-height: 200px; width:90%; margin-top:100%;",
+  },
+  {
+    id: 7,
+    title: "Bukti Kalau Kamera Juga Bisa Tersesat",
+    description:
+      "Kamera udah auto-focus, tapi dia tetap ngeblur. Karena bahkan lensa pun tahu: ini bukan pemandangan yang layak dilihat.</span>",
+    img: "./assets/img/a7.jpg",
+    link: "https://pvgnl.com/",
+  },
+  {
+    id: 6,
+    title: "Bukan Manusia, Bukan Alien, Tapi… Ya Gitu Deh",
+    description:
+      "Foto ini tidak bisa dikategorikan dalam taksonomi makhluk hidup manapun. Temanmu berhasil bikin spesies baru. </span>.",
+    img: "./assets/img/a8.jpg",
+    link: "https://xd.adobe.com/view/948b0329-3878-4714-9de8-2ef0b99ee092-c1d8/",
+  },
+  {
+    id: 5,
+    title: "Foto Ini Disensor oleh Nurani, Tapi Tetap Tayang",
+    description:
+      "Seharusnya foto ini masuk deep web, tapi karena persahabatan, kita upload. Jangan salahkan kami kalau mata kamu ngambek.</span>.",
+    img: "./assets/img/a9.jpg",
+    link: "https://www.figma.com/file/v7j37pvIsdXi8D4mo3sKWe/E-Waste-Management?type=design&node-id=18%3A112&mode=design&t=OndY7poaBzVae13M-1",
+  },
+  {
+    id: 4,
+    title: "Saat Semua Sudut Adalah Sudut Salah",
+    description:
+      "Biasanya ada ‘angle terbaik’, tapi dia berhasil membuktikan bahwa tidak ada satu pun angle yang bisa menyelamatkan wajah ini.</span>",
+    img: "./assets/img/a16.jpg",
+    link: "https://traveliaagency.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Gagal Jadi Idol, Sukses Jadi Meme",
+    description:
+      "Cita-citanya pengen viral karena prestasi, malah viral karena ekspresi absurd pas lagi begitu",
+    img: "./assets/img/a11.jpg",
+    link: "https://lovely-madeleine-ab3068.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Ekspresi Limited Edition, Nggak Ada di Dunia Nyata",
+    description:
+      " Mukanya sukses membentuk ekspresi yang bahkan belum dikenali oleh ilmu biologi. Kayaknya ini mutasi spontan.</span>",
+    img: "./assets/img/a12.jpg",
+    link: "https://cheery-madeleine-a3db79.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Ketika Wajah dan Kamera Tidak Sepakat",
+    description:
+      "Kamera bilang ‘senyum’, wajahnya bilang ‘krisis hidup’. Hasilnya? Foto yang bisa merusak mood satu angkatan.",
+    img: "./assets/img/a15.jpg",
+    link: "https://keen-bavarois-0eb8c4.netlify.app/",
+  },
+];
